e3c7846449fb406dca1749798c05209125130338
Stephen Paul Weber <singpolyma@singpolyma.net>
2007-12-08 15:43:23 +0000

    PHP4 compat issues

--- geobank/cibc-pc.php
+++ geobank/cibc-pc.php
@@ -27,13 +27,13 @@ function bank_infonow($province,$city,$bank) {
 	$url = $url[1];
 	preg_match('/jsessionid=.*/', $url, $sessid);
 	$sessid = $sessid[0];
-//	$url = $infonow_urls[$bank]['domain'].$url."?stateProvince=$province&city=$city&country=CAN";
-	$url = "http://192.168.0.101/?stateProvince=$province&city=$city&country=CAN";
+	$url = $infonow_urls[$bank]['domain'].$url."?stateProvince=$province&city=$city&country=CAN";
 	$result = '';
 
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+//	curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+	curl_setopt($ch, CURLOPT_REFERER, $url);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 	$result .= curl_exec($ch);
 	curl_close($ch);
