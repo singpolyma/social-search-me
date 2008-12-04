1d65ee3267bf1268f1b7f841501222bc6610f5a7
Stephen Paul Weber <singpolyma@singpolyma.net>
2008-12-03 23:36:38 -0500

    persist queue

--- crawl.rb
+++ crawl.rb
@@ -9,13 +9,17 @@ db = Mysql.new(db_settings[0],db_settings[1],db_settings[2],db_settings[3])
 db.reconnect = true
 db.real_query("SET NAMES 'UTF8'")
 
-$queue = ['http://singpolyma.net']
+$queue = open(ENV['HOME'] + '/.socialsearchmequeue').read.split(/\n/)
+$queue = ['http://singpolyma.net'] unless $queue and $queue.length > 0
 loop do
 	0.upto($queue.length-1) do |i|
 		item = $queue.shift
 		puts "Crawling #{item}..."
 		fetch_store_profile(item, db)
 	end
+	fh = File.new(ENV['HOME'] + '/.socialsearchmequeue', 'w')
+	fh.write($queue.join("\n"))
+	fh.close
 end
 
 db.close
